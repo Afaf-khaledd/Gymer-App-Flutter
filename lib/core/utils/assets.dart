d52dcdbb42340984a4da2931ec752a6a8806277a
Afaf-khaledd <afafkhaled56@gmail.com>
2025-02-01 15:02:16 +0200

    constants are added

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,7 @@
+class AssetsManager {
+  static const String basePath = 'Assets/';
+
+  //variables
+  static const String varName = '${basePath}anyImage.png';
+
+}
---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class ColorsManager {
+  // all colors here
+  static const Color mainGoldColor = Color.fromRGBO(215,179,101,0.61);
+  static const Color BGColor = Color.fromRGBO(35,35,35,1);
+
+}
