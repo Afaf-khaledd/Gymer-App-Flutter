d1e3e16d09a4a90f7afa72489a06d95c5e946d19
Afaf-khaledd <afafkhaled56@gmail.com>
2025-02-01 16:06:03 +0200

    black button component is added

---
+++ lib/core/components/customBlackButton.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../utils/colors.dart';
+
+class CustomBlackButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+  const CustomBlackButton({super.key, required this.label, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(50.0),
+      child: SizedBox(
+        width: double.infinity,
+        height: 52,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: ColorsManager.blackColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            elevation: 10,
+            shadowColor: Color.fromRGBO(3, 19, 20, 1),
+          ),
+          onPressed: onPressed,
+          child: Text(
+            label,
+            style: GoogleFonts.poppins(
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
