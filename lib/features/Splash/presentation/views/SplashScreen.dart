4bb7f4472ecf4f24e81e29972a5634aec6cedbd4
Afaf-khaledd <afafkhaled56@gmail.com>
2025-02-02 13:17:14 +0200

    first splash screen

--- lib/features/Splash/presentation/views/SplashScreen.dart
+++ lib/features/Splash/presentation/views/SplashScreen.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:gymer/core/utils/colors.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (_) => const Placeholder()));
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: ColorsManager.BGColor,
+      body: Center(
+        child: Text('GYMER',
+            style: GoogleFonts.poppins(
+                fontSize: 40,
+                height: 1.5,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w800,
+                color: ColorsManager.blackColor)),
+      ),
+    );
   }
 }
