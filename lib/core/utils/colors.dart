6fdfea0639716bf44ff3179402499b17d4859832
Afaf-khaledd <afafkhaled56@gmail.com>
2025-02-02 13:13:38 +0200

    padding in button is fixed

--- lib/core/utils/colors.dart
+++ lib/core/utils/colors.dart
@@ -2,7 +2,8 @@ import 'dart:ui';
 
 class ColorsManager {
   // all colors here
-  static const Color goldColor = Color.fromRGBO(215,179,101,0.61);
+  static const Color goldColorO1 = Color.fromRGBO(215,179,101,1);
+  static const Color goldColorO60 = Color.fromRGBO(215,179,101,0.61);
   static const Color blackColor = Color.fromRGBO(3, 19, 20, 1);
   static const Color BGColor = Color.fromRGBO(245, 245, 245, 1);
 
