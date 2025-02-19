e88df1c35e295b41fadc408ac4228ecf1683537e
Afaf-khaledd <afafkhaled56@gmail.com>
2025-02-19 15:37:29 +0200

    chatbot error message fixed

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:gymer/core/utils/colors.dart';
-import 'package:gymer/features/Chatbot/data/repository/chatRepo.dart';
-import 'package:gymer/features/Chatbot/data/repository/chatRepo.dart';
-import 'package:gymer/features/Chatbot/presentation/view%20model/chatCubit/chat_cubit.dart';
 import 'package:gymer/features/Chatbot/presentation/view%20model/chatCubit/chat_cubit.dart';
 import 'package:gymer/features/Splash/presentation/views/SplashScreen.dart';
 
@@ -52,6 +49,11 @@ class MyApp extends StatelessWidget {
         theme: ThemeData.light().copyWith(
             scaffoldBackgroundColor: ColorsManager.BGColor,
             focusColor: ColorsManager.blackColor,
+            textSelectionTheme: TextSelectionThemeData(
+              cursorColor: ColorsManager.goldColorO60,
+              selectionColor: ColorsManager.goldColorO60,
+              selectionHandleColor: ColorsManager.goldColorO60,
+            ),
             primaryColor: ColorsManager.blackColor),
         home: const SplashScreen(),
         builder: EasyLoading.init(),
